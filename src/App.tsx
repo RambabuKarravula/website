7eb650297f0a23705abdd74c064a9eb801ddf68f
Rambabu <114081878+RambabuKarravula@users.noreply.github.com>
2025-04-01 23:23:28 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -364,7 +364,7 @@ function App() {
               </p>
               <p className="flex items-center text-gray-400">
                 <MapPin className="w-5 h-5 mr-2" />
-                Hyderabad, India
+                Chennai, Tamil Nadu, India
               </p>
             </div>
           </div>
